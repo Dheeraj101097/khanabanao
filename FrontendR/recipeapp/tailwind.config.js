ab3f61564fa1e53c7bc1cede397807e8810d2b92
Dheeraj@5 <devishree1005@gmail.com>
2024-12-31 04:06:16 -0800

    Update tailwind.config.js

--- FrontendR/recipeapp/tailwind.config.js
+++ FrontendR/recipeapp/tailwind.config.js
@@ -12,7 +12,7 @@ export default {
   plugins: [],
 };
 
-import withMT from ("@material-tailwind/react/utils/withMT");
+import withMT from "@material-tailwind/react/utils/withMT";
 
 module.exports = withMT({
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
