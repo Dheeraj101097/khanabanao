127e85c6cfcc0d2f9febdfbe2277a15da3f238eb
Dheeraj@5 <devishree1005@gmail.com>
2025-01-11 10:21:58 -0800

    Update geminiapi.js

--- BackendR/geminiapi.js
+++ BackendR/geminiapi.js
@@ -5,7 +5,7 @@ const {
   HarmBlockThreshold,
 } = require("@google/generative-ai");
 
-const apiKey = "AIzaSyCZSjwFWRZNgQu4oInECldLTBx4Vrul66g";
+const apiKey = process.env.GEMINI_API_KEY
 const genAI = new GoogleGenerativeAI(apiKey);
 
 const model = genAI.getGenerativeModel({
