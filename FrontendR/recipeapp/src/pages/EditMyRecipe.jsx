4d18683f01ce154384a5e6d7d2bafa040a67eb26
Dheeraj@5 <devishree1005@gmail.com>
2025-01-29 03:30:18 -0800

    Update EditMyRecipe.jsx

--- FrontendR/recipeapp/src/pages/EditMyRecipe.jsx
+++ FrontendR/recipeapp/src/pages/EditMyRecipe.jsx
@@ -61,7 +61,7 @@ const EditMyRecipe = () => {
   //
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const url = `http://localhost:5000/auth/myRecipePage/${ID.id}`;
+    const url = `https://khanabanao-backendr.onrender.com/myRecipePage/${ID.id}`;
     try {
       const response = await fetch(url, {
         method: "PUT",
