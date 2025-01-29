c221a4ffc3cea65d2e83e6d832de0fbb5ee759db
Dheeraj@5 <devishree1005@gmail.com>
2025-01-29 03:31:24 -0800

    Update Signup.jsx

--- FrontendR/recipeapp/src/pages/Signup.jsx
+++ FrontendR/recipeapp/src/pages/Signup.jsx
@@ -35,7 +35,7 @@ function Signup() {
     //do try catch if name email pass entered
 
     try {
-      const url = "http://localhost:5000/auth/signup";
+      const url = "https://khanabanao-backendr.onrender.com/signup";
 
       const response = await fetch(url, {
         method: "POST",
