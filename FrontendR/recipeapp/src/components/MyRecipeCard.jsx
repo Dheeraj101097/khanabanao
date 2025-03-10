95c611e7c251936307b3973751539945e3614890
Dheeraj@5 <devishree1005@gmail.com>
2025-03-10 07:59:24 -0700

    Update MyRecipeCard.jsx

--- FrontendR/recipeapp/src/components/MyRecipeCard.jsx
+++ FrontendR/recipeapp/src/components/MyRecipeCard.jsx
@@ -36,48 +36,48 @@ const MyRecipeCard = ({ myDish, refreshRecipePage }) => {
   return (
     <>
       {myDish ? (
-      <div className="bg-white shadow-md rounded-lg overflow-hidden border border-gray-200 max-w-xs mx-auto mb-4 transition-transform transform hover:scale-105 hover:bg-opacity-10  hover:bg-gradient-to-t hover:from-yellow-100  hover:to-amber-300  ">
-        <img
-          className="h-48 w-full object-cover rounded-[2.5rem] p-2 "
-          src={myDish.image}
-          alt={myDish.title || "Recipe Image"}
-        />
-        <div className="p-4">
-          <h2 className="text-lg font-semibold text-gray-800">
-            {myDish.title.replace(/^./, (char) => char.toUpperCase()) ||
-              "Untitled Recipe"}
-          </h2>
-          <p className="text-sm text-gray-600 mt-1">
-            <strong>Category:</strong>{" "}
-            {myDish.category.replace(/^./, (char) => char.toUpperCase()) ||
-              "No Category"}
-          </p>
-          <p className="text-sm text-gray-600 mt-1">
-            <strong>Ingredients:</strong>{" "}
-            {myDish.ingredients.slice(0, 40) || "No Ingredients"}....
-          </p>
-          <p className="text-sm text-gray-600 mt-1">
-            <strong>Instructions:</strong>{" "}
-            {myDish.instructions.slice(0, 30) || "No Instructions"}....
-          </p>
-          <div className="flex mt-4">
-            <button className=" bg-[#1fd77b] text-white px-3 py-1 rounded-lg hover:bg-green-600 transition mr-2">
-              <Link to={`/ViewMyRecipe/${myDish._id}`}>View</Link>
-            </button>
-            <button className=" bg-[#3561ff] text-white px-3 py-1 rounded-lg hover:bg-blue-900 transition mr-2">
-              <Link to={`/EditMyRecipe/${myDish._id}`}>Edit</Link>
-            </button>
-            <button
-              className="bg-[#F00808] text-white px-3 py-1 rounded-lg hover:bg-red-900 transition"
-              onClick={() => {
-                handleDelete(myDish._id);
-              }}
-            >
-              Delete
-            </button>
+        <div className="bg-white shadow-md rounded-lg overflow-hidden border border-gray-200 max-w-xs mx-auto mb-4 transition-transform transform hover:scale-105 hover:bg-opacity-10  hover:bg-gradient-to-t hover:from-yellow-100  hover:to-amber-300  ">
+          <img
+            className="h-48 w-full object-cover rounded-[2.5rem] p-2 "
+            src={myDish.image}
+            alt={myDish.title || "Recipe Image"}
+          />
+          <div className="p-4">
+            <h2 className="text-lg font-semibold text-gray-800">
+              {myDish.title.replace(/^./, (char) => char.toUpperCase()) ||
+                "Untitled Recipe"}
+            </h2>
+            <p className="text-sm text-gray-600 mt-1">
+              <strong>Category:</strong>{" "}
+              {myDish.category.replace(/^./, (char) => char.toUpperCase()) ||
+                "No Category"}
+            </p>
+            <p className="text-sm text-gray-600 mt-1">
+              <strong>Ingredients:</strong>{" "}
+              {myDish.ingredients.slice(0, 40) || "No Ingredients"}....
+            </p>
+            <p className="text-sm text-gray-600 mt-1">
+              <strong>Instructions:</strong>{" "}
+              {myDish.instructions.slice(0, 30) || "No Instructions"}....
+            </p>
+            <div className="flex mt-4">
+              <button className=" bg-[#1fd77b] text-white px-3 py-1 rounded-lg hover:bg-green-600 transition mr-2">
+                <Link to={`/ViewMyRecipe/${myDish._id}`}>View</Link>
+              </button>
+              <button className=" bg-[#3561ff] text-white px-3 py-1 rounded-lg hover:bg-blue-900 transition mr-2">
+                <Link to={`/EditMyRecipe/${myDish._id}`}>Edit</Link>
+              </button>
+              <button
+                className="bg-[#F00808] text-white px-3 py-1 rounded-lg hover:bg-red-900 transition"
+                onClick={() => {
+                  handleDelete(myDish._id);
+                }}
+              >
+                Delete
+              </button>
+            </div>
           </div>
         </div>
-      </div>
       ) : (
         <LoadingEffect />
       )}
